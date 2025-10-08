84a0c52ae04b7cd36c8f567d5ed560935a14ade7
jaysh <jshelderfer@ufl.edu>
2025-10-08 16:02:27 -0400

    prettier

--- frontend/jest.config.ts
+++ frontend/jest.config.ts
@@ -3,13 +3,16 @@ import type { Config } from 'jest';
 const config: Config = {
   testEnvironment: 'jsdom',
   transform: {
-    '^.+\\.(ts|tsx)$': ['ts-jest', { 
-      tsconfig: {
-        jsx: 'react',
-        esModuleInterop: true,
-        moduleResolution: 'node'
-      }
-    }],
+    '^.+\\.(ts|tsx)$': [
+      'ts-jest',
+      {
+        tsconfig: {
+          jsx: 'react',
+          esModuleInterop: true,
+          moduleResolution: 'node',
+        },
+      },
+    ],
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json'],
   setupFilesAfterEnv: ['<rootDir>/test/setupTests.ts'],
