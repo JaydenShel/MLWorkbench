4ae10333ad2aeb86a8fdbac858d3f841fcdbf71b
jaysh <jshelderfer@ufl.edu>
2025-10-08 14:20:35 -0400

    csvUp1

---
+++ backend/services/storage.py
@@ -0,0 +1,19 @@
+from abc import ABC, abstractmethod
+from pathlib import Path
+import uuid
+
+class Storage(ABC):
+    @abstractmentod
+    async def save_bytes(self, filename: str, data: bytes) -> tuple[str, int]:
+        return (uri, size_bytes)
+
+class LocalStorage(Storage):
+    def __init__(self, base_dir: str = "data"):
+        Path(base_dir).mkdir(parents=True, exist_ok=True)
+        self.base_dir = Path(base_dir)
+
+    async def save_bytes(self, filename: str, data: bytes) -> tuple[str, int]:
+        key = f"{uuid.uuid4()}_{filename}"
+        path = self.base_dir / key
+        path.write_bytes(data)
+        return (f"file://{path.resolve()}", len(data))
